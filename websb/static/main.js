5c405d862aba615c848ed8e406de39041b939dea
Michał Rokita <mRokita@users.noreply.github.com>
2016-06-02 14:12:06 +0200

    Fix invalid URL

--- websb/static/main.js
+++ websb/static/main.js
@@ -1,6 +1,6 @@
 var app = angular.module("sb", []);
 app.controller("serverList", function($scope, $http){
-    $http.get("/api/v1/latest/").success(function(response){
+    $http.get("/api/v1/scans/latest/").success(function(response){
         $scope.scan = response;
         console.log($scope.scan);
     })
