fa0e193e2622971286df3e606ec7bc4cf3b46ed2
Michał Rokita <mRokita@user.noreply.github.com>
2016-06-02 22:45:59 +0200

    Add serverlist

--- websb/static/main.js
+++ websb/static/main.js
@@ -1,8 +1,16 @@
 var app = angular.module("sb", []);
-app.controller("serverList", function($scope, $http, $window){
-    $http.get("/api/v1/scans/latest/?show_variables=1").success(function(response){
+app.controller("serverList", function($scope, $http){
+    $http.get("/api/v1/scans/latest/?show_variables=1&show_players=1").success(function(response){
+        var rowWidth;
+        $scope.rowStyle = {"margin": "0px", 'padding': '0px'};
+        function getWidths(){
+            var rowWidth = $(".collapsible").width();
+            $scope.rowStyle['max-width'] = rowWidth/3-1+"px";
+            $scope.rowStyle['min-width'] = rowWidth/3-1+"px";
+        }
+        $(window).resize(getWidths);
+        $(document).ready(getWidths);
         $scope.scan = response;
-        $scope.rowStyle = $window.rowStyle;
         console.log($scope.scan);
     })
 });
