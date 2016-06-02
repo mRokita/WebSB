a92778f1472b680564116e721bc5037f72544616
Michał Rokita <mRokita@users.noreply.github.com>
2016-06-02 14:06:51 +0200

    Fix the bug

--- websb/static/main.js
+++ websb/static/main.js
@@ -2,6 +2,6 @@ var app = angular.module("sb");
 app.controller("serverList", function($scope, $http){
     $http.get("/api/v1/latest/").success(function(response){
         $scope.scan = response;
-        console.log($scope.scan)
+        console.log($scope.scan);
     })
 });
